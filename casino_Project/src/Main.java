33bedab267f92fa1d7a719def9edc50a72dcf313
DomenicoD17 <148124099+DomenicoD17@users.noreply.github.com>
2023-12-04 09:32:13 -0800

    class outline
    class outline

--- casino_Project/src/Main.java
+++ casino_Project/src/Main.java
@@ -1,5 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class card {
+    String suit;
+    String rank;
+
+    card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
+class deck{
+        ArrayList<card> cards;
+        deck(){
+            cards = new ArrayList<>();
+            String[] suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
+            String[] ranks = {"2","3","4","5","6","7","8","9","10","Jack","Queen", "King", "Ace"};
+        }
+
+}
+class player{
+        ArrayList<card> hand;
+}
+
 public class Main {
     public static void main(String[] args) {
 
